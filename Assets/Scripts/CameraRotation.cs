2cdceb11fec4c78918aff418c7355141312fec9a
yurihikari <quangminhjean@gmail.com>
2022-10-05 12:16:35 +0200

    Removed Cinemachine, added Working zoom without using FOV, better slider control

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -10,47 +10,72 @@ public class CameraRotation : MonoBehaviour
     public float minRotationX = 31;
     private Vector3 previousPosition;
 
-    //public bool fovZoom = false;
+    // Activate or not Zoom with FOV, take the Distance zoom otherwise
+    public bool fovZoom = false;
 
+    // Zoom Slider
     public Slider zoomSlider;
+
+    // Fov Zoom Settings
     public float minFovZoom = 1.0f;
     public float maxFovZoom = 90.0f;
     public float scrollSensitivity = 20.0f;
 
+    // Distance Zoom Settings
+    public Transform parentObject;
+    public float zoomLevel;
+    public float sensitivity=100f;
+    public float speed = 1000;
+    public float maxZoom = 100;
+    public float zoomPosition;
+
+    // Clipping Settings
+    public GameObject[] clippingTargetList;
+
     private float fov;
 
     void Start() {
-        zoomSlider.minValue = minFovZoom;
-        zoomSlider.maxValue = maxFovZoom;
+        if(fovZoom) {
+            zoomSlider.minValue = minFovZoom;
+            zoomSlider.maxValue = maxFovZoom;
+        }
+        else {
+            zoomSlider.minValue = 0;
+            zoomSlider.maxValue = maxZoom * 10;
+            zoomSlider.value = 0;
+        }
+        
     }
  
 
     void Update()
     {
         // FOV Zoom method
-        //if(fovZoom) {
-        fov = cam.fieldOfView;
-        fov -= Input.GetAxis("Mouse ScrollWheel") * scrollSensitivity;
-        fov = Mathf.Clamp(fov, minFovZoom, maxFovZoom);
-        zoomSlider.value = 0 + fov;
-        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, fov, Time.deltaTime);
-        //}
-        // else {
-        //     if (Input.GetAxis("Mouse ScrollWheel") > 0) {
-        //         cam.transform.position = new Vector3(transform.position.x, transform.position.y-.6f, transform.position.z+.2f);
-        //         //transform.Rotate(-1, 0, 0);
-        //     }
-        //     if (Input.GetAxis("Mouse ScrollWheel") < 0) {
-        //         cam.transform.position = new Vector3(transform.position.x, transform.position.y+.6f, transform.position.z-.2f);
-        //         //transform.Rotate(1, 0, 0);
-        //     }
-        // }
-
-        // float scrollAxis = Input.GetAxis ("Mouse ScrollWheel");
-        // if ( Input.GetAxis("Mouse ScrollWheel") > 0) {     
-        //     cam.transform.Translate(Vector3.forward * Time.deltaTime * 10000f * scrollAxis, Space.Self );
-        // }
-
+        if(fovZoom) {
+            fov = cam.fieldOfView;
+            fov -= Input.GetAxis("Mouse ScrollWheel") * scrollSensitivity;
+            fov = Mathf.Clamp(fov, minFovZoom, maxFovZoom);
+            zoomSlider.value = 0 + fov;
+            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, fov, Time.deltaTime);
+        }
+        // Distance Zoom
+        else {
+            // If scrollwheel is used, set the zoom level based on the scroll delta y
+            if(Input.GetAxis("Mouse ScrollWheel") != 0) {
+                zoomLevel += Input.mouseScrollDelta.y * sensitivity;
+                zoomLevel = Mathf.Clamp(zoomLevel, 0, maxZoom);
+            }
+            // Else use the slider value to define the zoom Level
+            else 
+            {
+                zoomLevel = zoomSlider.value / 10;
+            }
+            zoomPosition = Mathf.MoveTowards(zoomPosition, zoomLevel, speed * Time.deltaTime);
+            transform.position = parentObject.position + (transform.forward * zoomPosition);
+            zoomSlider.value = zoomLevel * 10;
+        }
+        
+        // If Left Mouse button, previous position takes the mouse
         if (Input.GetMouseButtonDown(0))
         {
             previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
@@ -63,13 +88,13 @@ public class CameraRotation : MonoBehaviour
             float rotationAroundYAxis = -direction.x * 180; // camera moves horizontally
             float rotationAroundXAxis = direction.y * 180; // camera moves vertically
             
-            cam.transform.position = target.position;
+            parentObject.transform.position = target.position;
 
-            cam.transform.Rotate(new Vector3(1, 0, 0), rotationAroundXAxis);
+            parentObject.transform.Rotate(new Vector3(1, 0, 0), rotationAroundXAxis);
 
-            cam.transform.Rotate(new Vector3(0, 1, 0), rotationAroundYAxis, Space.World); // <— This is what makes it work!
+            parentObject.transform.Rotate(new Vector3(0, 1, 0), rotationAroundYAxis, Space.World); // <— This is what makes it work!
             
-            cam.transform.Translate(new Vector3(0, 0, -distanceToTarget));
+            parentObject.transform.Translate(new Vector3(0, 0, -distanceToTarget));
 
             previousPosition = newPosition;
         }
@@ -78,5 +103,10 @@ public class CameraRotation : MonoBehaviour
                 //cam.transform.Rotate(new Vector3(1, 0, 0), -rotationAroundXAxis);
                 cam.transform.rotation = Quaternion.Euler(31,cam.transform.eulerAngles.y , cam.transform.eulerAngles.z);
         }
+
+        // foreach(GameObject object in clippingTargetList)
+        // {
+        //     if()
+        // }
     }
 }
--- Assets/Zoom.cs
+++ Assets/Zoom.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
-
+using UnityEngine.UI;
 public class Zoom : MonoBehaviour
 {
     public CinemachineVirtualCamera virtualCamera;
     public CinemachineComponentBase cinemachineComponentBase;
     public float cameraDistance = 1f;
     public float zoomSensibility = 0.2f;
-    public float maxZoom = 100f;
-    public float minZoom = 1f;
+    public float maxZoom = 23f;
+    public float minZoom = 3f;
+    public Slider zoomSlider;
+    private float oldSliderValue = 35; 
+
+    public float zoomOffset = 0f; // Above 0 if there is a value difference between slider and zoom value
     // Start is called before the first frame update
     void Start()
     {
-        
+        oldSliderValue = 1;
+        zoomSlider.value = 1;
     }
 
     // Update is called once per frame
@@ -25,21 +30,48 @@ public class Zoom : MonoBehaviour
         }
         if (Input.GetAxis("Mouse ScrollWheel") != 0) {
             cameraDistance = Input.GetAxis("Mouse ScrollWheel");
-            if(cinemachineComponentBase is CinemachineFramingTransposer) {
-                // if(cameraDistance > maxZoom)
-                //     (cinemachineComponentBase as CinemachineFramingTransposer).m_CameraDistance = maxZoom;
-                // else 
+            if (cinemachineComponentBase is CinemachineFramingTransposer) {
+                if(zoomSlider.value - cameraDistance * 10 > maxZoom) {
+                    (cinemachineComponentBase as CinemachineFramingTransposer).m_CameraDistance -= 0;
+                    zoomSlider.value = maxZoom;
+                }
+                else if (zoomSlider.value - cameraDistance * 10 < minZoom) {
+                    (cinemachineComponentBase as CinemachineFramingTransposer).m_CameraDistance -= 0;
+                    zoomSlider.value = minZoom;
+                }
+                else {
                     (cinemachineComponentBase as CinemachineFramingTransposer).m_CameraDistance -= cameraDistance * zoomSensibility;
+                    zoomSlider.value -= cameraDistance * 10;
+                }
+                oldSliderValue = zoomSlider.value;
+                Debug.Log("Slider : " + (zoomSlider.value - cameraDistance * 10));
+                Debug.Log("cameraDistance : " + (cinemachineComponentBase as CinemachineFramingTransposer).m_CameraDistance);
             }
         }
-        // if (Input.GetAxis("Mouse ScrollWheel") < 0) {
-        //     cameraDistance = Input.GetAxis("Mouse ScrollWheel") * zoomSensibility;
-        //     if(cinemachineComponentBase is CinemachineFramingTransposer) {
-        //         if(cameraDistance < minZoom)
-        //             (cinemachineComponentBase as CinemachineFramingTransposer).m_CameraDistance = minZoom;
-        //         else 
-        //             (cinemachineComponentBase as CinemachineFramingTransposer).m_CameraDistance += cameraDistance;
-        //     }
+        else {
+            cameraDistance = (zoomSlider.value + zoomOffset )/10;
+            if (cinemachineComponentBase is CinemachineFramingTransposer) {
+                if(zoomSlider.value > maxZoom) {
+                    zoomSlider.value = maxZoom;
+                }
+                else if (zoomSlider.value < minZoom) {
+                    zoomSlider.value = minZoom;
+                }
+                else {
+                    (cinemachineComponentBase as CinemachineFramingTransposer).m_CameraDistance = cameraDistance;
+                }
+                oldSliderValue = zoomSlider.value;
+                Debug.Log("Slider : " + (zoomSlider.value));
+                Debug.Log("cameraDistance : " + (cinemachineComponentBase as CinemachineFramingTransposer).m_CameraDistance);
+            }
+        }
+        // Code for the slider change
+        // if (zoomSlider.value > oldSliderValue) {
+        //     (cinemachineComponentBase as CinemachineFramingTransposer).m_CameraDistance -= 0.1f;
         // }
+        // if (zoomSlider.value < oldSliderValue) {
+        //     (cinemachineComponentBase as CinemachineFramingTransposer).m_CameraDistance += 0.1f;
+        // }
+        // oldSliderValue = (cinemachineComponentBase as CinemachineFramingTransposer).m_CameraDistance *10;
     }
 }
