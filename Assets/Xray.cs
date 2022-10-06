4c58eb31b2768408ab4852c0b7d63ef0b24b4c06
yurihikari <quangminhjean@gmail.com>
2022-10-06 15:02:41 +0200

    Added "Xray" selection tab.

---
+++ Assets/Xray.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+public class Xray : MonoBehaviour
+{
+    public List<GameObject> xrayTargetList = new List<GameObject>();
+    private List<Material> xrayTargetOldMaterial = new List<Material>();
+    public List<Toggle> xrayTargetToggleList = new List<Toggle>();
+    public List<GameObject> hideTargetList = new List<GameObject>();
+
+    public Material xrayMaterial;
+    public UnityEvent onXrayEnabled;
+    public UnityEvent onXrayDisabled;
+    private Toggle currentToggle;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentToggle = GetComponent<Toggle>();
+        foreach(GameObject targetObject in xrayTargetList) {
+            xrayTargetOldMaterial.Add(targetObject.GetComponent<Renderer>().material);
+        }
+        currentToggle.onValueChanged.AddListener(delegate {
+            if(currentToggle.isOn) {
+                activateXray();
+                foreach(Toggle toggle in xrayTargetToggleList)
+                {
+                    toggle.interactable = false;
+                }
+            }
+            else
+            {
+                disableXray();
+                foreach(Toggle toggle in xrayTargetToggleList)
+                {
+                    toggle.interactable = true;
+                }
+            }
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if(currentToggle.isOn) {
+        //     onXrayEnabled.Invoke();
+        // }
+        // else
+        // {
+        //     onXrayDisabled.Invoke();
+        // }
+    }
+
+    public void activateXray() {
+        foreach(GameObject targetObject in xrayTargetList) {
+            xrayTargetOldMaterial.Add(targetObject.GetComponent<Renderer>().material);
+            targetObject.GetComponent<Renderer>().material = xrayMaterial;
+        }
+        foreach(GameObject targetObject in hideTargetList) {
+            targetObject.SetActive(false);
+        }
+    }
+
+    public void disableXray() {
+        for (int index = 0; index < xrayTargetList.Count; index++) {
+            xrayTargetList.ToArray()[index].GetComponent<Renderer>().material = xrayTargetOldMaterial.ToArray()[index];
+        }
+        foreach(GameObject targetObject in hideTargetList) {
+            targetObject.SetActive(true);
+        }
+    }
+    
+
+}
